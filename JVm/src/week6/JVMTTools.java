e885c659819ba8515846cdfbccbf4fbd503538cb
ftteotw <i@iwvwi.com>
2016-10-12 23:50:39 +0800

    hah

---
+++ JVm/src/week6/JVMTTools.java
@@ -0,0 +1,20 @@
+package week6;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JVMTTools {
+	
+	public static void createlist(int count)throws InterruptedException{
+		for(int i=0;i<count;i++){
+			List<_1MObject>list=new ArrayList<_1MObject>();
+			Thread.sleep(100);
+			list.add(new _1MObject());
+		}
+	}
+    public static void main(String[] args)throws InterruptedException{
+    	createlist(5000);
+    }
+}
+
+// -verbose:gc -Xms40m -Xmx40m -Xmn10m -XX:+PrintGCDetails -XX:SurvivorRatio=8
---
+++ JVm/src/week6/JavaTest1.java
@@ -0,0 +1,10 @@
+package week6;
+
+public class JavaTest1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   System.out.println("hahahahhdfdasfafasfdfffffffff");
+	}
+
+}
