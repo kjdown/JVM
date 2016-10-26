ea33616c7c378d41e0fb8d1b0f7188c785853a90
ftteotw <i@iwvwi.com>
2016-10-27 01:21:28 +0800

    2

---
+++ JavaVMStackOOM.java
@@ -0,0 +1,21 @@
+public class JavaVMStackOOM1{
+    private void dontStop(){
+	whiel(true){
+	}
+    }
+
+    public void stackLeakByThread(){
+	while(true){
+	    Thread thread=new Thread(new Runnable(){
+
+		    public void run(){
+			dontStop);
+		    )
+		});
+	thread.start();
+    }
+}
+public static void main(String[]args)throws Throwable{
+    JavaVMStackOOM1 oom=new JavaVMStackOOM();
+    oom.stackLeakByThread();
+}}
--- RuntimeConstantPoolOOM.java
+++ RuntimeConstantPoolOOM.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class RuntimeConstantPoolOOM{
     public static void main(String[]args){
 	List<String>list=new ArrayList<String>();
